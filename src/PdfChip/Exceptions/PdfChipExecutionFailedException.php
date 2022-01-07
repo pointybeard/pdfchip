a6f354d9016102abaf951a884e485cc05368a3ba
Alannah Kearney <hi@alannahkearney.com>
2022-01-07 13:04:31 +1000

    chore: Update header copyright line

--- src/PdfChip/Exceptions/PdfChipExecutionFailedException.php
+++ src/PdfChip/Exceptions/PdfChipExecutionFailedException.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the "PHP Wrapper for callas pdfChip" repository.
  *
- * Copyright 2021 Alannah Kearney <hi@alannahkearney.com>
+ * Copyright 2021-22 Alannah Kearney <hi@alannahkearney.com>
  *
  * For the full copyright and license information, please view the LICENCE
  * file that was distributed with this source code.
